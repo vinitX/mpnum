Fix automatic compression in normalization
see mparray_test.test_normalization_compression
for the details

mpnum/mparray.py
@@ -232,16 +232,19 @@ class MPArray(object):
     ################################
     # FIXME Maybe we should extract site-normalization logic to seperate funcs
     def normalize(self, **kwargs):
-        """Brings the MPA to canonnical form in place.
+        """Brings the MPA to canonnical form in place. Note that we do not
+        support full left- or right-normalization. The right- (left- resp.)
+        most local tensor is not normalized since this can be done by
+        simply calculating its norm (instead of using SVD)
 
         [Sch11, Sec. 4.4]
 
         Possible combinations:
             normalize() = normalize(left=len(self) - 1)
                 -> full left-normalization
-            normalize(left=m) for 0 <= m < len(self) - 1
+            normalize(left=m) for 0 <= m <= len(self) - 1
                 -> self[0],..., self[m-1] are left-normalized
-            normalize(right=n) for 0 < n < len(self)
+            normalize(right=n) for 0 < n <= len(self)
                 -> self[n],..., self[-1] are right-normalized
             normalize(left=m, right=n) valid for m < n
                 -> self[0],...,self[m-1] are left normalized and
@@ -277,9 +280,11 @@ class MPArray(object):
         for site in range(lnormal, to_site):
             ltens = self._ltens[site]
             matshape = (np.prod(ltens.shape[:-1]), ltens.shape[-1])
-            unitary, triangle = qr(ltens.reshape(matshape))
-            self._ltens[site][:] = unitary.reshape(ltens.shape)
-            self._ltens[site + 1][:] = matdot(triangle, self._ltens[site + 1])
+            q, r = qr(ltens.reshape(matshape))
+            # if ltens.shape[-1] > prod(ltens.phys_shape) --> trivial comp.
+            # can be accounted by adapting bond dimension here
+            self._ltens[site] = q.reshape(ltens.shape[:-1] + (-1, ))
+            self._ltens[site + 1] = matdot(r, self._ltens[site + 1])
 
         self._lnormalized = to_site
         self._rnormalized = max(to_site + 1, rnormal)
@@ -299,8 +304,10 @@ class MPArray(object):
             ltens = self._ltens[site]
             matshape = (ltens.shape[0], np.prod(ltens.shape[1:]))
             q, r = qr(ltens.reshape(matshape).T)
-            self._ltens[site][:] = q.T.reshape(ltens.shape)
-            self._ltens[site - 1][:] = matdot(self._ltens[site - 1], r.T)
+            # if ltens.shape[-1] > prod(ltens.phys_shape) --> trivial comp.
+            # can be accounted by adapting bond dimension here
+            self._ltens[site] = q.T.reshape((-1, ) + ltens.shape[1:])
+            self._ltens[site - 1] = matdot(self._ltens[site - 1], r.T)
 
         self._lnormalized = min(to_site - 1, lnormal)
         self._rnormalized = to_site
@@ -331,10 +338,10 @@ class MPArray(object):
             direction = kwargs.pop('direction', default_direction)
 
             if direction == 'right':
-                self.normalize(left=0, right=1)
+                self.normalize(right=1)
                 return self._compress_svd_r(max_bdim, **kwargs)
             elif direction == 'left':
-                self.normalize(left=len(self) - 1, right=len(self))
+                self.normalize(left=len(self) - 1)
                 return self._compress_svd_l(max_bdim, **kwargs)
         else:
             raise ValueError("{} is not a valid method.".format(method))

tests/mparray_test.py
@@ -112,8 +112,11 @@ def test_inner_mat(nr_sites, local_dim, bond_dim):
 
 @pt.mark.parametrize('nr_sites, local_dim, bond_dim', MP_TEST_PARAMETERS)
 def test_norm(nr_sites, local_dim, bond_dim):
-    psi = factory.random_mpa(nr_sites, local_dim, bond_dim)
-    assert_almost_equal(mp.inner(psi, psi), mp.norm(psi)**2)
+    mp_psi = factory.random_mpa(nr_sites, local_dim, bond_dim)
+    psi = mp_psi.to_array()
+
+    assert_almost_equal(mp.inner(mp_psi, mp_psi), mp.norm(mp_psi)**2)
+    assert_almost_equal(np.sum(psi.conj() * psi), mp.norm(mp_psi)**2)
 
 
 @pt.mark.parametrize('nr_sites, local_dim, bond_dim', MP_TEST_PARAMETERS)
@@ -131,11 +134,12 @@ def test_add_and_subtr(nr_sites, local_dim, bond_dim):
 def test_mult_mpo_scalar(nr_sites, local_dim, bond_dim):
     mpo = factory.random_mpa(nr_sites, (local_dim, local_dim), bond_dim)
     op = mpo_to_global(mpo)
+    scalar = np.random.randn()
 
-    assert_array_almost_equal(2 * op, mpo_to_global(2 * mpo))
+    assert_array_almost_equal(scalar * op, mpo_to_global(scalar * mpo))
 
-    mpo *= 2
-    assert_array_almost_equal(2 * op, mpo_to_global(mpo))
+    mpo *= scalar
+    assert_array_almost_equal(scalar * op, mpo_to_global(mpo))
 
 
 ###############################################################################
@@ -210,19 +214,56 @@ def test_jump_normalization(nr_sites, local_dim, bond_dim):
 
 
 @pt.mark.parametrize('nr_sites, local_dim, bond_dim', MP_TEST_PARAMETERS)
+def test_full_normalization(nr_sites, local_dim, bond_dim):
+    mpo = factory.random_mpa(nr_sites, (local_dim, local_dim), bond_dim)
+    op = mpo_to_global(mpo)
+    assert_correct_normalzation(mpo, 0, nr_sites)
+    assert_array_almost_equal(op, mpo_to_global(mpo))
+
+    mpo.normalize(right=1)
+    assert_correct_normalzation(mpo, 0, 1)
+    assert_array_almost_equal(op, mpo_to_global(mpo))
+
+    ###########################################################################
+    mpo = factory.random_mpa(nr_sites, (local_dim, local_dim), bond_dim)
+    op = mpo_to_global(mpo)
+    assert_correct_normalzation(mpo, 0, nr_sites)
+    assert_array_almost_equal(op, mpo_to_global(mpo))
+
+    mpo.normalize(left=len(mpo) - 1)
+    assert_correct_normalzation(mpo, len(mpo) - 1, len(mpo))
+    assert_array_almost_equal(op, mpo_to_global(mpo))
+
+
+def test_normalization_compression():
+    """If the bond dimension is too large at the boundary, qr decompostion
+    in normalization may yield smaller bond dimension"""
+    mpo = factory.random_mpa(sites=2, ldim=2, bdim=20)
+    mpo.normalize(right=1)
+    assert_correct_normalzation(mpo, 0, 1)
+    assert mpo.bdims[0] == 2
+
+    mpo = factory.random_mpa(sites=2, ldim=2, bdim=20)
+    mpo.normalize(left=1)
+    assert_correct_normalzation(mpo, 1, 2)
+    assert mpo.bdims[0] == 2
+
+
+@pt.mark.parametrize('nr_sites, local_dim, bond_dim', MP_TEST_PARAMETERS)
 def test_mult_mpo_scalar_normalization(nr_sites, local_dim, bond_dim):
     mpo = factory.random_mpa(nr_sites, (local_dim, local_dim), bond_dim)
     op = mpo_to_global(mpo)
+    scalar = np.random.randn()
 
     center = nr_sites // 2
     mpo.normalize(left=center - 1, right=center)
-    mpo_times_two = 2 * mpo
+    mpo_times_two = scalar * mpo
 
-    assert_array_almost_equal(2 * op, mpo_to_global(mpo_times_two))
+    assert_array_almost_equal(scalar * op, mpo_to_global(mpo_times_two))
     assert_correct_normalzation(mpo_times_two, center - 1, center)
 
-    mpo *= 2
-    assert_array_almost_equal(2 * op, mpo_to_global(mpo))
+    mpo *= scalar
+    assert_array_almost_equal(scalar * op, mpo_to_global(mpo))
     assert_correct_normalzation(mpo, center - 1, center)
 
 
@@ -237,6 +278,7 @@ def test_compression_svd(nr_sites, local_dim, bond_dim):
         assert_equal(bdims[0] + bdims[1], bdims[2])
 
     # Right-compression
+    mpo_new = mpo + zero
     err = mpo_new.compress(max_bdim=bond_dim, method='svd', direction='right')
     assert_array_equal(mpo_new.bdims, bond_dim)
     assert_array_almost_equal(mpo_to_global(mpo), mpo_to_global(mpo_new))
@@ -244,7 +286,7 @@ def test_compression_svd(nr_sites, local_dim, bond_dim):
     assert_almost_equal(err, 0.)
 
     # Left-compression
-    # mpo_new = mpo + zero
+    mpo_new = mpo + zero
     err = mpo_new.compress(max_bdim=bond_dim, method='svd', direction='left')
     assert_array_equal(mpo_new.bdims, bond_dim)
     assert_array_almost_equal(mpo_to_global(mpo), mpo_to_global(mpo_new))