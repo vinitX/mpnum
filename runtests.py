f9aff519db5cf8167c29b2c87989621d0cf7c360
Milan Holzaepfel <mail@mjh.name>
2015-11-26 12:10:20 +0100

    do not test whether numba JIT returns correct results by default

--- runtests.py
+++ runtests.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 
+import os
 import os.path
 import sys
 
@@ -8,6 +9,10 @@ import pytest
 
 
 def main():
+    # Disable numba JIT optimization, which will take a couple of seconds (i.e.
+    # as long as the tests are supposed to take).  This must be done before
+    # importing numba.
+    os.environ['NUMBA_DISABLE_JIT'] = '1'
     # Run doctests afterwards because they auto-import things. We want to see
     # missing imports.
     pytest.main('tests tests_photonic_tomo')
