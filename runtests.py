ea6431839e050916f0ba47b1f79bd30a676d49f7
Daniel Suess <daniel@dsuess.me>
2015-12-31 14:24:11 +0100

    Add functionality to pass arguments to runtests

--- runtests.py
+++ runtests.py
@@ -8,18 +8,16 @@ import sys
 import pytest
 
 
-def main():
+def main(*args):
     # Disable numba JIT optimization, which will take a couple of seconds (i.e.
     # as long as the tests are supposed to take).  This must be done before
     # importing numba.
     os.environ['NUMBA_DISABLE_JIT'] = '1'
     # Run doctests afterwards because they auto-import things. We want to see
     # missing imports.
-    pytest.main('tests tests_photonic_tomo')
-    pytest.main('--doctest-modules mpnum photonic_tomo')
+    pytest.main(list(args) + ['tests', 'tests_photonic_tomo'])
+    pytest.main(['--doctest-modules', 'mpnum', 'photonic_tomo'])
 
 
 if __name__ == '__main__':
-    main()
-
-
+    main(*sys.argv[1:])
