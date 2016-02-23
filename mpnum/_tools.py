1a041104d693bfa24a98efde0b2e983dedfdc32e
Daniel Suess <daniel@dsuess.me>
2016-02-23 14:56:28 +0100

    Remove defintion of Paulis in _tools

--- mpnum/_tools.py
+++ mpnum/_tools.py
@@ -12,14 +12,6 @@ import numpy as np
 
 from six.moves import range, zip
 
-SI = np.eye(2)
-SX = np.array([[0, 1], [1, 0]])
-SY = np.array([[0, 1j], [-1j, 0]])
-SZ = np.array([[1, 0], [0, -1]])
-SP = 0.5 * (SX + 1j * SY)
-SM = 0.5 * (SX - 1j * SY)
-PAULI = np.asarray([SI, SX, SY, SZ])
-
 
 def global_to_local(array, sites):
     """Converts a general `sites`-local array with fixed number p of physical
