d9d7ca2f4931fd209086efffce4b43fe02c7b3a4
Milan Holzaepfel <mail@mjh.name>
2016-08-22 10:25:30 +0200

    Workaround for pytest 3.0.0 bug

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,9 @@ import pytest
 @pytest.fixture(scope="module")
 def rgen():
     return numpy.random.RandomState(seed=52973992)
+
+
+@pytest.fixture(scope='function', autouse=True)
+def bug_workaround():
+    # Workaround for https://github.com/pytest-dev/pytest/issues/1832
+    pass
