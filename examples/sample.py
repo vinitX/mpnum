4dac099c228ea8225e8b363d4547ad7a606ee4a0
Milan Holzaepfel <mail@mjh.name>
2016-08-26 12:01:49 +0200

    Refactor MPPovm.sample()
    Implementation is still incomplete.

--- examples/sample.py
+++ examples/sample.py
@@ -4,9 +4,9 @@ import numpy as np
 import mpnum as mp
 import mpnum.povm as povm
 
-n_sites = 8
+n_sites = 40
 ldim = 2
-n_samples = 20
+n_samples = 100
 eps = 1e-10
 
 rng = np.random.RandomState(seed=3770537836)
@@ -18,5 +18,5 @@ rho = mp.mps_to_mpo(psi)
 locpovm = povm.x_povm(ldim)
 mpp = povm.MPPovm.from_local_povm(locpovm, n_sites)
 
-s = mpp.sample(rng, rho, n_samples, direct_max_n_probab=-1, eps=eps)
+s = mpp.sample(rng, rho, n_samples, 'cond', n_group=6, eps=eps)
 print(s)
