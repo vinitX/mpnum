1a3e8bbdbba728afdd9fbb652b11fd4d1ac838a9
Milan Holzaepfel <mail@mjh.name>
2016-08-31 14:23:45 +0200

    Add block_pauli_povmlist() and tiled_pauli_povmlist()

--- mpnum/povm/__init__.py
+++ mpnum/povm/__init__.py
@@ -1,4 +1,5 @@
 # encoding: utf-8
 
-from .localpovm import POVM, pauli_povm, x_povm, y_povm, z_povm
-from .mppovm import MPPovm, MPPovmList
+from .localpovm import POVM, pauli_parts, pauli_povm, x_povm, y_povm, z_povm
+from .mppovm import MPPovm, MPPovmList, block_pauli_povmlist, \
+    tiled_pauli_povmlist
