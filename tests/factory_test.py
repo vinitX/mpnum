0b02c748ae11ff38958649fdbeccfa4f9ca04112
Daniel Suess <daniel@dsuess.me>
2016-09-02 12:30:03 +0200

    Add factory.diagonal_mpa

--- tests/factory_test.py
+++ tests/factory_test.py
@@ -7,7 +7,10 @@ import numpy as np
 import pytest as pt
 
 import mpnum.factory as factory
-import numpy as np
+from numpy.testing import assert_array_almost_equal
+
+from mparray_test import MP_TEST_PARAMETERS, MP_TEST_DTYPES
+from mpnum._testing import assert_correct_normalization
 
 
 @pt.mark.parametrize('nr_sites, local_dim, bond_dim', [(2, 3, 3), (3, 2, 4),
@@ -20,3 +23,25 @@ def test_mpdo_positivity(nr_sites, local_dim, bond_dim, rgen):
     np.testing.assert_array_almost_equal(rho_dense, rho_dense.conj().T)
     lambda_min = min(np.real(np.linalg.eigvals(rho_dense)))
     assert lambda_min > -1e-14, "{} < -1e-14".format(lambda_min)
+
+
+#  @pt.mark.parametrize('dtype', MP_TEST_DTYPES)
+@pt.mark.parametrize('nr_sites, local_dim, _', MP_TEST_PARAMETERS)
+def test_diagonal_mpa(nr_sites, local_dim, _, rgen, dtype=np.float_):
+    randfunc = factory._randfuncs[dtype]
+    entries = randfunc((local_dim,), randstate=rgen)
+
+    mpa_mp = factory.diagonal_mpa(entries, nr_sites)
+    if nr_sites > 1:
+        mpa_np = np.zeros((local_dim,) * nr_sites)
+        np.fill_diagonal(mpa_np, entries)
+    else:
+        mpa_np = entries
+
+    assert len(mpa_mp) == nr_sites
+    assert_array_almost_equal(mpa_mp.to_array(), mpa_np)
+    assert mpa_mp.dtype is dtype
+    assert_correct_normalization(mpa_mp, nr_sites - 1, nr_sites)
+
+    if nr_sites > 1:
+        assert mpa_mp.bdim == local_dim
