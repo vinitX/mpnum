6f07da5d25753a049e7347e38542e99cd231bb6c
Daniel Suess <daniel@dsuess.me>
2016-09-07 14:59:30 +0200

    Implement hierarchical sum-compression

--- tests/special_test.py
+++ tests/special_test.py
@@ -17,9 +17,12 @@ from mpnum._tools import global_to_local
 from mparray_test import MP_TEST_DTYPES, MP_TEST_PARAMETERS
 
 MP_INNER_PARAMETERS = [(10, 10, 5), (20, 2, 10)]
-MP_SUMUP_PARAMETERS = [(6, 2, 1000, 100, 200), (10, 2, 500, 5, 20)]
+MP_SUMUP_PARAMETERS = [(6, 2, 5000, 10, 200), (10, 2, 5000, 5, 20)]
 
 
+############################
+#  special.inner_prod_mps  #
+############################
 @pt.mark.parametrize('dtype', MP_TEST_DTYPES)
 @pt.mark.parametrize('nr_sites, local_dim, bond_dim', MP_TEST_PARAMETERS)
 def test_inner_prod_mps(nr_sites, local_dim, bond_dim, dtype, rgen):
@@ -69,3 +72,54 @@ def test_inner_slow(nr_sites, local_dim, bond_dim, benchmark, rgen):
                               randstate=rgen)
 
     benchmark(mp.inner, mpa1, mpa2)
+
+
+########################
+#  special.sumup_prod  #
+########################
+@pt.mark.parametrize('nr_sites, local_dim, bond_dim', MP_TEST_PARAMETERS)
+def test_sumup(nr_sites, local_dim, bond_dim, rgen):
+    bond_dim = bond_dim if bond_dim is not np.nan else 1
+    mpas = [factory.random_mpa(nr_sites, local_dim, 1, dtype=np.float_, randstate=rgen)
+            for _ in range(10 * bond_dim)]
+    weights = rgen.randn(len(mpas))
+
+    # parameters chosen such that only one round of compression occurs
+    summed_fast = mpsp.sumup(mpas, weights=weights, target_bdim=bond_dim,
+                             max_bdim=10 * bond_dim)
+    summed_slow = mp.sumup(mpa * w for mpa, w in zip(mpas, weights))
+    summed_slow.compress('svd', bdim=bond_dim)
+    assert_mpa_identical(summed_fast, summed_slow)
+
+    try:
+        mpsp.sumup(mpas, weights=np.ones(bond_dim))
+    except AssertionError:
+        pass
+    else:
+        raise AssertionError("sumup did not catch unbalanced arguments")
+
+
+@pt.mark.long
+@pt.mark.benchmark(group="sumup", max_time=10)
+@pt.mark.parametrize('nr_sites, local_dim, samples, target_bdim, max_bdim', MP_SUMUP_PARAMETERS)
+def test_sumup_fast(nr_sites, local_dim, samples, target_bdim, max_bdim, rgen, benchmark):
+    mpas = [factory.random_mpa(nr_sites, local_dim, 1, dtype=np.float_, randstate=rgen)
+            for _ in range(samples)]
+    weights = rgen.randn(len(mpas))
+
+    benchmark(mpsp.sumup, mpas, weights=weights, target_bdim=target_bdim,
+              max_bdim=max_bdim)
+
+
+@pt.mark.long
+@pt.mark.benchmark(group="sumup", max_time=10)
+@pt.mark.parametrize('nr_sites, local_dim, samples, target_bdim, _', MP_SUMUP_PARAMETERS)
+def test_sumup_slow(nr_sites, local_dim, samples, target_bdim, _, rgen, benchmark):
+    mpas = [factory.random_mpa(nr_sites, local_dim, 1, dtype=np.float_, randstate=rgen)
+            for _ in range(samples)]
+    weights = rgen.randn(len(mpas))
+
+    @benchmark
+    def sumup_slow():
+        summed = mp.sumup(mpa * w for w, mpa in zip(weights, mpas))
+        summed.compress('svd', bdim=target_bdim)
