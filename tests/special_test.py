7a6aaa15c09a00ba831d51d7b902155fe82a02a7
Daniel Suess <daniel@dsuess.me>
2016-09-07 11:25:15 +0200

    Split up tests/benchmarks

--- tests/special_test.py
+++ tests/special_test.py
@@ -14,23 +14,22 @@ from mpnum._testing import (assert_correct_normalization,
                             assert_mpa_almost_equal, assert_mpa_identical,
                             mpo_to_global)
 from mpnum._tools import global_to_local
-from mparray_test import MP_TEST_DTYPES
+from mparray_test import MP_TEST_DTYPES, MP_TEST_PARAMETERS
 
-# nr_sites, local_dim, bond_dim
-MP_BENCHMARK_PARAMETERS = [(10, 10, 5), (20, 2, 10)]
+MP_INNER_PARAMETERS = [(10, 10, 5), (20, 2, 10)]
+MP_SUMUP_PARAMETERS = [(6, 2, 1000, 100, 200), (10, 2, 500, 5, 20)]
 
 
-@pt.mark.long
 @pt.mark.parametrize('dtype', MP_TEST_DTYPES)
-@pt.mark.parametrize('nr_sites, local_dim, bond_dim', MP_BENCHMARK_PARAMETERS)
-def test_inner_fast(nr_sites, local_dim, bond_dim, dtype, benchmark, rgen):
+@pt.mark.parametrize('nr_sites, local_dim, bond_dim', MP_TEST_PARAMETERS)
+def test_inner_prod_mps(nr_sites, local_dim, bond_dim, dtype, rgen):
     mpa1 = factory.random_mpa(nr_sites, local_dim, 1, dtype=dtype,
                               randstate=rgen, normalized=True)
     mpa2 = factory.random_mpa(nr_sites, local_dim, bond_dim, dtype=dtype,
                               randstate=rgen, normalized=True)
 
     res_slow = mp.inner(mpa1, mpa2)
-    res_fast = benchmark(mpsp.inner_prod_mps, mpa1, mpa2)
+    res_fast = mpsp.inner_prod_mps(mpa1, mpa2)
     assert_almost_equal(res_slow, res_fast)
 
     try:
@@ -38,7 +37,8 @@ def test_inner_fast(nr_sites, local_dim, bond_dim, dtype, benchmark, rgen):
     except AssertionError:
         pass
     else:
-        raise AssertionError("inner_prod_mps should only accept bdim=1 in first argument")
+        if bond_dim > 1:
+            raise AssertionError("inner_prod_mps should only accept bdim=1 in first argument")
 
     mpa1 = factory.random_mpo(nr_sites, local_dim, 1)
     try:
@@ -49,13 +49,23 @@ def test_inner_fast(nr_sites, local_dim, bond_dim, dtype, benchmark, rgen):
         raise AssertionError("inner_prod_mps should only accept plegs=1")
 
 
-@pt.mark.long
-@pt.mark.parametrize('dtype', MP_TEST_DTYPES)
-@pt.mark.parametrize('nr_sites, local_dim, bond_dim', MP_BENCHMARK_PARAMETERS)
-def test_inner_slow(nr_sites, local_dim, bond_dim, dtype, benchmark, rgen):
-    mpa1 = factory.random_mpa(nr_sites, local_dim, 1, dtype=dtype,
+@pt.mark.benchmark(group="inner")
+@pt.mark.parametrize('nr_sites, local_dim, bond_dim', MP_INNER_PARAMETERS)
+def test_inner_fast(nr_sites, local_dim, bond_dim, benchmark, rgen):
+    mpa1 = factory.random_mpa(nr_sites, local_dim, 1, dtype=np.float_,
+                              randstate=rgen, normalized=True)
+    mpa2 = factory.random_mpa(nr_sites, local_dim, bond_dim, dtype=np.float_,
+                              randstate=rgen, normalized=True)
+
+    benchmark(mpsp.inner_prod_mps, mpa1, mpa2)
+
+
+@pt.mark.benchmark(group="inner")
+@pt.mark.parametrize('nr_sites, local_dim, bond_dim', MP_INNER_PARAMETERS)
+def test_inner_slow(nr_sites, local_dim, bond_dim, benchmark, rgen):
+    mpa1 = factory.random_mpa(nr_sites, local_dim, 1, dtype=np.float_,
                               randstate=rgen)
-    mpa2 = factory.random_mpa(nr_sites, local_dim, bond_dim, dtype=dtype,
+    mpa2 = factory.random_mpa(nr_sites, local_dim, bond_dim, dtype=np.float_,
                               randstate=rgen)
 
     benchmark(mp.inner, mpa1, mpa2)
