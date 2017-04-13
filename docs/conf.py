7b2e1aaba63bcd9c46bba5cb0010655fc81df9f5
Milan Holzaepfel <mail@mjh.name>
2017-04-13 17:29:32 +0200

    Docs: Custom CSS for links in headings

--- docs/conf.py
+++ docs/conf.py
@@ -120,7 +120,7 @@ html_theme = 'alabaster'
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+#html_short_title = u'mpnum docs'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -134,8 +134,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
-html_static_path = []
+html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
