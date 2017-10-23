f439367131d25876f0179349a86556e5889bb791
Milan Holzaepfel <mail@mjh.name>
2017-10-23 16:33:38 +0200

    Re-run SVG to PNG conversion after reverting SVG files

--- docs/img_convert.sh
+++ docs/img_convert.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 
-convert -density 120 mpnum_logo.svg mpnum_logo_120.png
-convert -density 144 mpnum_logo.svg mpnum_logo_144.png
+convert -background 'rgba(255,255,255,0)' -density 120 mpnum_logo.svg mpnum_logo_120.png 
+convert -background 'rgba(255,255,255,0)' -density 144 mpnum_logo.svg mpnum_logo_144.png 
 
-for i in mpnum_*.svg tensors_*.svg; do
-  convert -density 100 $i ${i/.svg/.png}
+for i in tensors_*; do
+  convert -background 'rgba(255,255,255,0)' -density 135 "$i" "${i/.svg/.png}"
 done
 
